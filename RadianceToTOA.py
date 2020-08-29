ce2b65efed57c7ac8640afa338bedc8c9d993861
KrisJCarroll <kjcarroll@alaska.edu>
2020-08-29 15:22:05 -0800

    added initial code to read raster band data and parse metadata from included XML

---
+++ RadianceToTOA.py
@@ -0,0 +1,20 @@
+import rasterio
+import numpy as np
+from xml.dom import minidom
+filepath = "Unortho Deering Images With RPCs 1-30/files/PSScene4Band/20160908_212941_0e0f/basic_analytic/"
+img_filename = "20160908_212941_0e0f_1B_AnalyticMS.tif"
+xml_filename = "20160908_212941_0e0f_1B_AnalyticMS_metadata.xml"
+
+img = filepath + img_filename
+xml_file = filepath + xml_filename
+
+# loading bands in radiance
+with rasterio.open(img) as src:
+    blue_band_radiance = src.read(1)
+    green_band_radiance = src.read(2)
+    red_band_radiance = src.read(3)
+    nir_band_radiance = src.read(4)
+
+# parsing the XML metadata to determine coefficients
+xmldoc = minidom.parse(xml_file)
+nodes = xmldoc.getElementsByTagName("ps:bandSpecificMetadata")
