55b1ebdea6b8160ea2434b994521f0b5a87d334d
KrisJCarroll <kjcarroll@alaska.edu>
2020-09-01 20:13:32 -0800

    updated code to function definition instead of standalone script

--- RadianceToTOA.py
+++ RadianceToTOA.py
@@ -1,134 +1,144 @@
 import warnings
+import os
 import rasterio
 warnings.filterwarnings("ignore", category=rasterio.errors.NotGeoreferencedWarning)
 import numpy as np
 from xml.dom import minidom
 
+def DN_to_TOA(rasterfile, xmlfile):
+    raster_filepath = os.path.dirname(rasterfile) + "/"
+    raster_filename = os.path.basename(rasterfile)
+    xml_filepath = os.path.dirname(xmlfile) + "/"
+    xml_filename = os.path.basename(xmlfile)
 
-filepath = "Unortho Deering Images With RPCs 1-30/files/PSScene4Band/20160908_212941_0e0f/basic_analytic/"
-img_filename = "20160908_212941_0e0f_1B_AnalyticMS.tif"
-xml_filename = "20160908_212941_0e0f_1B_AnalyticMS_metadata.xml"
+    img = raster_filepath + raster_filename
+    xml_file = xml_filepath + xml_filename
 
-img = filepath + img_filename
-xml_file = filepath + xml_filename
+    # the following code for converting to TOA reflectance was largely adapted from a tutorial on Planet
+    # source: https://developers.planet.com/tutorials/convert-planetscope-imagery-from-radiance-to-reflectance/
+    # loading bands in radiance
+    print("Opening", raster_filename, "to read in band data:", end=" ")
+    with rasterio.open(img, driver="GTiff") as src:
+        kwargs = src.meta
+        kwargs.update(dtype=rasterio.uint16, count=4)
+        print("DONE")
+        print("\tReading BLUE:", end=" ")
+        blue_band_radiance = src.read(1)   # band 1 - blue
+        print("DONE")
 
-# loading bands in radiance
-print("Opening", img_filename, "to read in band data:", end=" ")
-with rasterio.open(img, driver="GTiff") as src:
-    kwargs = src.meta
-    kwargs.update(dtype=rasterio.uint16, count=4)
+        print("\tReading GREEN:", end=" ")
+        green_band_radiance = src.read(2)  # band 2 - green
+        print("DONE")
+
+        print("\tReading RED:", end=" ")
+        red_band_radiance = src.read(3)    # band 3 - red
+        print("DONE")
+
+        print("\tReading NIR:", end=" ")
+        nir_band_radiance = src.read(4)    # band 4 - near-infrared
+        print("DONE")
+    print("\tBlue band:")
+    print("\t\tMIN: {} MAX: {}".format(np.amin(blue_band_radiance), np.amax(blue_band_radiance)))
+    print("\tGreen band:")
+    print("\t\tMIN: {} MAX: {}".format(np.amin(green_band_radiance), np.amax(green_band_radiance)))
+    print("\tRed band:")
+    print("\t\tMIN: {} MAX: {}".format(np.amin(red_band_radiance), np.amax(red_band_radiance)))
+    print("\tNIR band:")
+    print("\t\tMIN: {} MAX: {}".format(np.amin(nir_band_radiance), np.amax(nir_band_radiance)))
+    print()
+
+    # parsing the XML metadata to determine coefficients
+    print("Parsing", xml_filename, "for reflectance coefficients:", end=" ")
+    xmldoc = minidom.parse(xml_file)
+    nodes = xmldoc.getElementsByTagName("ps:bandSpecificMetadata")
+    coeffs = {}
+    for node in nodes:
+        band_num = node.getElementsByTagName("ps:bandNumber")[0].firstChild.data
+        if band_num in ['1', '2', '3', '4']:
+            i = int(band_num)
+            value = node.getElementsByTagName("ps:reflectanceCoefficient")[0].firstChild.data
+            coeffs[i] = float(value)
     print("DONE")
-    print("\tReading BLUE:", end=" ")
-    blue_band_radiance = src.read(1)   # band 1 - blue
+
+    print("\tReflectance coefficients:")
+    print("\t\tBLUE: {}".format(coeffs[1]))
+    print("\t\tGREEN: {}".format(coeffs[2]))
+    print("\t\tRED: {}".format(coeffs[3]))
+    print("\t\tNIR: {}".format(coeffs[4]))
+    print()
+
+    # converting Digital Number (DN) to TOA reflectance
+    print("Converting to top-of-atmosphere (TOA) reflectance:", end=" ")
+    blue_band_reflectance = blue_band_radiance * coeffs[1]
+    green_band_reflectance = green_band_radiance * coeffs[2]
+    red_band_reflectance = red_band_radiance * coeffs[3]
+    nir_band_reflectance = nir_band_radiance * coeffs[4]
     print("DONE")
 
-    print("\tReading GREEN:", end=" ")
-    green_band_radiance = src.read(2)  # band 2 - green
+    print("\tBlue band:")
+    print("\t\tMIN: {} MAX: {}".format(np.amin(blue_band_reflectance), np.amax(blue_band_reflectance)))
+    print("\tGreen band:")
+    print("\t\tMIN: {} MAX: {}".format(np.amin(green_band_reflectance), np.amax(green_band_reflectance)))
+    print("\tRed band:")
+    print("\t\tMIN: {} MAX: {}".format(np.amin(red_band_reflectance), np.amax(red_band_reflectance)))
+    print("\tNIR band:")
+    print("\t\tMIN: {} MAX: {}".format(np.amin(nir_band_reflectance), np.amax(nir_band_reflectance)))
+    print()
+
+    # writing the TOA reflectance image to disk
+    scale = 100000
+    out_filename = raster_filename.split(sep=".")[0] + "_TOAreflectance.tif"
+    print("Saving TOA reflectance as", out_filename, ":", end=" ")
+    scaled_blue = blue_band_reflectance * scale
+    scaled_green = green_band_reflectance * scale
+    scaled_red = red_band_reflectance * scale
+    scaled_nir = nir_band_reflectance * scale
+    with rasterio.open(raster_filepath + out_filename, 'w', **kwargs) as dst:
+        dst.write_band(1, scaled_blue.astype(rasterio.uint16))
+        dst.write_band(2, scaled_green.astype(rasterio.uint16))
+        dst.write_band(3, scaled_red.astype(rasterio.uint16))
+        dst.write_band(4, scaled_nir.astype(rasterio.uint16))
     print("DONE")
 
-    print("\tReading RED:", end=" ")
-    red_band_radiance = src.read(3)    # band 3 - red
-    print("DONE")
+    import matplotlib.pyplot as plt
+    import matplotlib.colors as colors
 
-    print("\tReading NIR:", end=" ")
-    nir_band_radiance = src.read(4)    # band 4 - near-infrared
-    print("DONE")
-print("\tBlue band:")
-print("\t\tMIN: {} MAX: {}".format(np.amin(blue_band_radiance), np.amax(blue_band_radiance)))
-print("\tGreen band:")
-print("\t\tMIN: {} MAX: {}".format(np.amin(green_band_radiance), np.amax(green_band_radiance)))
-print("\tRed band:")
-print("\t\tMIN: {} MAX: {}".format(np.amin(red_band_radiance), np.amax(red_band_radiance)))
-print("\tNIR band:")
-print("\t\tMIN: {} MAX: {}".format(np.amin(nir_band_radiance), np.amax(nir_band_radiance)))
-print()
+    class MidpointNormalize(colors.Normalize):
+        """
+        Normalise the colorbar so that diverging bars work there way either side from a prescribed midpoint value)
+        e.g. im=ax1.imshow(array, norm=MidpointNormalize(midpoint=0.,vmin=-100, vmax=100))
+        Credit: Joe Kington, http://chris35wills.github.io/matplotlib_diverging_colorbar/
+        """
+        def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+            self.midpoint = midpoint
+            colors.Normalize.__init__(self, vmin, vmax, clip)
 
-# parsing the XML metadata to determine coefficients
-print("Parsing", xml_filename, "for reflectance coefficients:", end=" ")
-xmldoc = minidom.parse(xml_file)
-nodes = xmldoc.getElementsByTagName("ps:bandSpecificMetadata")
-coeffs = {}
-for node in nodes:
-    band_num = node.getElementsByTagName("ps:bandNumber")[0].firstChild.data
-    if band_num in ['1', '2', '3', '4']:
-        i = int(band_num)
-        value = node.getElementsByTagName("ps:reflectanceCoefficient")[0].firstChild.data
-        coeffs[i] = float(value)
-print("DONE")
+        def __call__(self, value, clip=None):
+            # I'm ignoring masked values and all kinds of edge cases to make a
+            # simple example...
+            x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+            return np.ma.masked_array(np.interp(value, x, y), np.isnan(value))
 
-print("\tReflectance coefficients:")
-print("\t\tBLUE: {}".format(coeffs[1]))
-print("\t\tGREEN: {}".format(coeffs[2]))
-print("\t\tRED: {}".format(coeffs[3]))
-print("\t\tNIR: {}".format(coeffs[4]))
-print()
+    for refl_band, color in zip([blue_band_reflectance, green_band_reflectance, red_band_reflectance, nir_band_reflectance],
+                    ["Blue", "Green", "Red", "NIR"]):
+        min_val = np.nanmin(refl_band)
+        max_val = np.nanmax(refl_band)
+        mid = np.nanmean(refl_band)
 
-# converting Digital Number (DN) to TOA reflectance
-print("Converting to top-of-atmosphere (TOA) reflectance:", end=" ")
-blue_band_reflectance = blue_band_radiance * coeffs[1]
-green_band_reflectance = green_band_radiance * coeffs[2]
-red_band_reflectance = red_band_radiance * coeffs[3]
-nir_band_reflectance = nir_band_radiance * coeffs[4]
-print("DONE")
+        fig = plt.figure(figsize=(20,10))
+        ax = fig.add_subplot(111)
+        cax = ax.imshow(refl_band, cmap='Greys', clim=(min_val, max_val),
+                    norm=MidpointNormalize(midpoint=mid, vmin=min_val, vmax=max_val))
 
-print("\tBlue band:")
-print("\t\tMIN: {} MAX: {}".format(np.amin(blue_band_reflectance), np.amax(blue_band_reflectance)))
-print("\tGreen band:")
-print("\t\tMIN: {} MAX: {}".format(np.amin(green_band_reflectance), np.amax(green_band_reflectance)))
-print("\tRed band:")
-print("\t\tMIN: {} MAX: {}".format(np.amin(red_band_reflectance), np.amax(red_band_reflectance)))
-print("\tNIR band:")
-print("\t\tMIN: {} MAX: {}".format(np.amin(nir_band_reflectance), np.amax(nir_band_reflectance)))
-print()
+        ax.axis('off')
+        ax.set_title(color + " Reflectance", fontsize=18, fontweight='bold')
 
-# writing the TOA reflectance image to disk
-scale = 100000
-filename = img_filename.split(sep=".")[0] + "_reflectance.tif"
-print("Saving TOA reflectance as", filename, ":", end=" ")
-scaled_blue = blue_band_reflectance * scale
-scaled_green = green_band_reflectance * scale
-scaled_red = red_band_reflectance * scale
-scaled_nir = nir_band_reflectance * scale
-with rasterio.open(filepath + filename, 'w', **kwargs) as dst:
-    dst.write_band(1, scaled_blue.astype(rasterio.uint16))
-    dst.write_band(2, scaled_green.astype(rasterio.uint16))
-    dst.write_band(3, scaled_red.astype(rasterio.uint16))
-    dst.write_band(4, scaled_nir.astype(rasterio.uint16))
-print("DONE")
+        cbar = fig.colorbar(cax, orientation='horizontal', shrink=0.65)
 
-import matplotlib.pyplot as plt
-import matplotlib.colors as colors
+        plt.show()
 
-class MidpointNormalize(colors.Normalize):
-    """
-    Normalise the colorbar so that diverging bars work there way either side from a prescribed midpoint value)
-    e.g. im=ax1.imshow(array, norm=MidpointNormalize(midpoint=0.,vmin=-100, vmax=100))
-    Credit: Joe Kington, http://chris35wills.github.io/matplotlib_diverging_colorbar/
-    """
-    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
-        self.midpoint = midpoint
-        colors.Normalize.__init__(self, vmin, vmax, clip)
+raster = "Unortho Deering Images With RPCs 1-30/files/PSScene4Band/20160908_212941_0e0f/basic_analytic/20160908_212941_0e0f_1B_AnalyticMS.tif"
 
-    def __call__(self, value, clip=None):
-        # I'm ignoring masked values and all kinds of edge cases to make a
-        # simple example...
-        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
-        return np.ma.masked_array(np.interp(value, x, y), np.isnan(value))
+xml = "Unortho Deering Images With RPCs 1-30/files/PSScene4Band/20160908_212941_0e0f/basic_analytic/20160908_212941_0e0f_1B_AnalyticMS_metadata.xml"
 
-for refl_band, color in zip([blue_band_reflectance, green_band_reflectance, red_band_reflectance, nir_band_reflectance],
-                ["Blue", "Green", "Red", "NIR"]):
-    min_val = np.nanmin(refl_band)
-    max_val = np.nanmax(refl_band)
-    mid = np.nanmean(refl_band)
-
-    fig = plt.figure(figsize=(20,10))
-    ax = fig.add_subplot(111)
-    cax = ax.imshow(refl_band, cmap='Greys', clim=(min_val, max_val),
-                norm=MidpointNormalize(midpoint=mid, vmin=min_val, vmax=max_val))
-
-    ax.axis('off')
-    ax.set_title(color + " Reflectance", fontsize=18, fontweight='bold')
-
-    cbar = fig.colorbar(cax, orientation='horizontal', shrink=0.65)
-
-    plt.show()
+DN_to_TOA(raster, xml)
