c0ccd9df7f3cd287cf42046beb1e1a7a7ac2a271
KrisJCarroll <kjcarroll@alaska.edu>
2020-09-22 16:51:18 -0800

    implemented searching for valid files to process

--- batchprocess.py
+++ batchprocess.py
@@ -1,7 +1,20 @@
 import os
-import rastertools
+import sys
+# import rastertools
 
+rootdir = os.getcwd()
 # this script will handle batch processing of files including:
 #       * parsing date information from filenames
 #       * gathering associated metadata for each image
 #       * outputting finished files into a new directory structure
+files_to_be_processed = []
+for root, subdirs, files in os.walk(rootdir + "/data"):
+    #print("Root: {} Subdirs: {} Files: {}".format(root, subdirs, files)
+    if files:
+        for f in files:
+            if f.endswith(".tif") and f.find("_SR_") != -1:
+                files_to_be_processed.append(f)
+
+files_to_be_processed.sort()
+for f in files_to_be_processed:
+    print(f)
