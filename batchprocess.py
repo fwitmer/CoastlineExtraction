d74ecd0b7de2b255ca61bb6e6cecdf180475bae3
KrisJCarroll <kjcarroll@alaska.edu>
2020-09-22 16:22:01 -0800

    initial commit

---
+++ batchprocess.py
@@ -0,0 +1,7 @@
+import os
+import rastertools
+
+# this script will handle batch processing of files including:
+#       * parsing date information from filenames
+#       * gathering associated metadata for each image
+#       * outputting finished files into a new directory structure
