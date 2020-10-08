e40ce439ff67d28fd6cfee1b0b4db4398fd3154f
KrisJCarroll <kjcarroll@alaska.edu>
2020-10-08 11:55:18 -0800

    added initial merge process

--- batchprocess.py
+++ batchprocess.py
@@ -3,6 +3,7 @@ import sys
 import datetime
 import rastertools as rt
 import rasterio
+from rasterio.merge import merge
 
 rootdir = os.getcwd()
 # this script will handle batch processing of files including:
@@ -47,7 +48,7 @@ if files_to_be_processed:
             file_groups[file_year][file_month] = {}
         if file_day not in file_groups[file_year][file_month].keys():
             file_groups[file_year][file_month][file_day] = []
-        file_groups[file_year][file_month][file_day].append(f[1])
+        file_groups[file_year][file_month][file_day].append(f[0])
 
 
         # process directory structure if not already made
@@ -56,11 +57,27 @@ if files_to_be_processed:
         if not os.path.isdir("data/output/{}/{}".format(file_year, file_month)):
             os.mkdir("data/output/{}/{}".format(file_year, file_month))
 
-        outfile_base = f[1][:15] + "_AnalyticMS_SR"
+        # outfile_base = f[1][:15] + "_AnalyticMS_SR"
         
-        ndwi_outfile = "data/output/{}/{}/{}".format(file_year, file_month, outfile_base + "_NDWI.tif")
-        ndwi_class_outfile = "data/output/{}/{}/{}".format(file_year, file_month, outfile_base + "_NDWI_classified.tif")
+        # ndwi_outfile = "data/output/{}/{}/{}".format(file_year, file_month, outfile_base + "_NDWI.tif")
+        # ndwi_class_outfile = "data/output/{}/{}/{}".format(file_year, file_month, outfile_base + "_NDWI_classified.tif")
 
-        ndwi = rt.calculate_ndwi(f[0], ndwi_outfile, plot=False)
-        ndwi_class = rt.ndwi_classify(f[0], ndwi_class_outfile, plot=False)
-        
+        # ndwi = rt.calculate_ndwi(f[0], ndwi_outfile, plot=False)
+        # ndwi_class = rt.ndwi_classify(f[0], ndwi_class_outfile, plot=False)
+    
+    for year, month_grp in file_groups.items():
+        for month, day_grp in file_groups[year].items():
+            for day, files in file_groups[year][month].items():
+                files_to_be_merged = []
+                for f in files:
+                    src = rasterio.open(f)
+                    files_to_be_merged.append(src)
+                mosaic, out_transform = merge(files_to_be_merged)
+                out_meta = src.meta.copy()
+                out_meta.update({"driver": "GTiff",
+                                 "height": mosaic.shape[1],
+                                 "width": mosaic.shape[2],
+                                 "transform": out_transform})
+                out_fp = "data/output/{year}/{month}/{year}{month}{day}_AnalyticMS_SR_merged.tif".format(year=year, month=month, day=day)
+                with rasterio.open(out_fp, "w", **out_meta) as dst:
+                    dst.write(mosaic)
