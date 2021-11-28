d6ce06280fb1d4638a466bc0d81806c61626cd7b
Jack Eisenrich Carroll <58920063+JackEisenrichCarroll@users.noreply.github.com>
2021-11-27 21:07:57 -0900

    Update process_snap_data.py
    Final version of SNAP process that was used to bulk process SNAP climate data.

--- process_snap_data.py
+++ process_snap_data.py
@@ -6,30 +6,31 @@ from scipy import interpolate
 import os
 import pandas as pd
 from datetime import datetime, date, timedelta
+import time
 import math
 
 # WKT Used to convert Lat/Lon to projection used in snap data
-CRS_WKT='PROJCS["unnamed",GEOGCS["unnamed ellipse",DATUM["unknown",SPHEROID["unnamed",6370000,0]],PRIMEM["Greenwich",' \
-        '0],UNIT["degree",0.0174532925199433]],PROJECTION["Polar_Stereographic"],PARAMETER["latitude_of_origin",64],' \
-        'PARAMETER["central_meridian",-152],PARAMETER["scale_factor",1],PARAMETER["false_easting",0],' \
-        'PARAMETER["false_northing",0],UNIT["Meter",1],EXTENSION["PROJ4","+units=m +proj=stere +lat_ts=64.0 ' \
-        '+lon_0=-152.0 +lat_0=90.0 +x_0=0 +y_0=0 +a=6370000 +b=6370000 +wktext"]] '
+CRS_WKT = 'PROJCS["unnamed",GEOGCS["unnamed ellipse",DATUM["unknown",SPHEROID["unnamed",6370000,0]],PRIMEM["Greenwich",' \
+          '0],UNIT["degree",0.0174532925199433]],PROJECTION["Polar_Stereographic"],PARAMETER["latitude_of_origin",64],' \
+          'PARAMETER["central_meridian",-152],PARAMETER["scale_factor",1],PARAMETER["false_easting",0],' \
+          'PARAMETER["false_northing",0],UNIT["Meter",1],EXTENSION["PROJ4","+units=m +proj=stere +lat_ts=64.0 ' \
+          '+lon_0=-152.0 +lat_0=90.0 +x_0=0 +y_0=0 +a=6370000 +b=6370000 +wktext"]] '
 
 # Center of region in lat/lon
 REGION_CENTER = (66.0756, -162.7172)
 
 # Filepaths for SNAP data
-TSK_FILEPATH = 'tsk_hourly_wrf_NCAR-CCSM4_historical_2005.nc'
-U10_FILEPATH = 'u10_hourly_wrf_NCAR-CCSM4_rcp85_2021.nc'
-V10_FILEPATH = 'v10_hourly_wrf_NCAR-CCSM4_rcp85_2021.nc'
-SEAICE_FILEPATH = 'TEMP'
-PSFC_FILEPATH = 'TEMP'
-TRANSECT_FILEPATH = 'WestChukchi_exposed_STepr_rates.shp'
+
+TSK_FILEPATH = '/usr/local/coastal/snap_processing/snap_data/tsk'
+U10_FILEPATH = '/usr/local/coastal/snap_processing/snap_data/u10'
+V10_FILEPATH = '/usr/local/coastal/snap_processing/snap_data/v10'
+SEAICE_FILEPATH = '/usr/local/coastal/snap_processing/snap_data/seaice'
+PSFC_FILEPATH = '/usr/local/coastal/snap_processing/snap_data/psfc'
+T2_FILEPATH = '/usr/local/coastal/snap_processing/snap_data/t2'
+TRANSECT_FILEPATH = '/usr/local/coastal/snap_processing/transect_data/WestChukchi_exposed_STepr_rates.shp'
 
 # Filepath for I/O csv data
-# TODO: Change these back to daily
-INPUT_FILEPATH = 'SNAP_daily_by_transect.csv'
-OUTPUT_FILEPATH = 'SNAP_daily_by_transect.csv'
+OUTPUT_FILEPATH = '/usr/local/coastal/snap_processing/snap_output/SNAP_daily_by_transect'
 
 # Create global variables
 global transformer_to_snap_proj
@@ -54,11 +55,36 @@ def get_transect_points(filepath):
     return transect_points, transects
 
 
+# Function to create pandas dataframe to store all data in
+def create_dataframe(transects, y):
+
+    start_date = date(y, 1, 1)
+    end_date = date(y, 12, 31)
+    delta = timedelta(days=1)
+
+    data = []
+
+    print(f'Creating dataframe for {y}')
+
+    while start_date <= end_date:
+
+        for i in range(0, len(transects)):
+            temp_transorder = transects.iloc[i]['TransOrder']
+            temp_date = datetime.strftime(start_date, '%m-%d-%Y')
+            temp_ID = str(temp_date) + '_' + str(temp_transorder)
+            data.append((temp_date, temp_transorder, temp_ID, 0, 0, 0, 0, 0, 0))
+
+        start_date += delta
+
+    df = pd.DataFrame(data, columns=['date', 'transect', 'ID', 'tsk', 'u10', 'v10', 'seaice', 'psfc', 't2'])
+
+    return df
+
+
 # Function that will import SNAP data and return array with all datasets
 # filepath can be either a nc file or a directory containing nc files
 # All files in filepath must contain the same SNAP data variable (tsk, u10, v10, etc.)
 def read_data(filepath):
-
     # Variable to store return tsk data
     data = []
 
@@ -66,7 +92,7 @@ def read_data(filepath):
     if os.path.isdir(filepath):
         for filename in os.scandir(filepath):
             try:
-                data.append(nc.Dataset(filename))
+                data.append(nc.Dataset(filepath + '/' + filename.name))
             except OSError:
                 pass
 
@@ -95,7 +121,6 @@ def read_data(filepath):
 # Method to get the closest coordinate to REGION_CENTER
 # Input should be a nc Dataset with SNAP data parameters
 def get_closest_coords(ds):
-
     arr = np.array(ds['xc'])
     difference_array = np.absolute(arr - mod_center[0])
     closest_x = difference_array.argmin()
@@ -110,11 +135,10 @@ def get_closest_coords(ds):
 # Crop SNAP surface temperature data to Deering region
 # (3 x coordinates and 3 y coordinates to make the 9 points closest to Deering)
 def crop_snap(ds, data, closest_x, closest_y):
-
     # Storage var declaration
     modified_data = np.empty((len(data), 3, 3))
-    ordered_coords = []
     i_ct = 0
+    ordered_coords = []
 
     for i in range(closest_y - 1, closest_y + 2):
         j_ct = 0
@@ -130,7 +154,6 @@ def crop_snap(ds, data, closest_x, closest_y):
 
 # Method to downscale snap data from hourly to daily
 def downscale_data(modified_data):
-
     # Declare storage var
     downscaled_data = np.empty(((int(len(modified_data) / 24)), len(modified_data[0]), len(modified_data[0][0])))
 
@@ -161,73 +184,76 @@ def finalize_data(downscaled_data):
 
 
 # Method that processes and saves data
-def process_data(data, data_name, dataframe, transects):
+def process_data(data, data_name, dataframe, transects, ordered_coords, y):
 
     for ds in data:
-
-        # Find X/Y coordinate on SNAP data grid closest do Deering center
-        closest_x, closest_y = get_closest_coords(ds)
-
-        # Separate tsks from rest of SNAP data
-        data = ds[data_name][:]
-
-        # Crop SNAP surface temperature data to Deering region
-        modified_data, ordered_coords = crop_snap(ds, data, closest_x, closest_y)
-
-        # Downscale surface temperature from hourly to daily #
-        downscaled_data = downscale_data(modified_data)
-
-        # Transform data from SNAP projection to lat/lon
-        for i in range(len(ordered_coords)):
-            ordered_coords[i] = transformer_to_lat_lon.transform(ordered_coords[i][0], ordered_coords[i][1])
-
-        # Reformat downscaled_data to work with ordered_coords for interpolation
-        finalized_data = finalize_data(downscaled_data)
 
         # Grab year from data
         year = int(ds.__dict__['reference_time'][0:4])
 
-        # Initializing start date
-        strt_date = date(int(year), 1, 1)
+        # Check corresponding year
+        if year == y:
 
-        # Interpolate this year's tsk data
-        for i in range(0, len(finalized_data)):
+            # Find X/Y coordinate on SNAP data grid closest do Deering center
+            closest_x, closest_y = get_closest_coords(ds)
 
-            # Get date from day
-            day_num = str(i)
-            day_num.rjust(3 + len(day_num), '0')
+            # Separate variable from rest of SNAP data (Time cost ~20 sec/ds)
+            current_data = ds[data_name][:]
 
-            res_date = strt_date + timedelta(days=int(day_num))
-            final_date = res_date.strftime("%m-%d-%Y")
+            # Crop SNAP surface temperature data to Deering region
+            if not ordered_coords:
+                # Extract data with coords
+                modified_data, ordered_coords = crop_snap(ds, current_data, closest_x, closest_y)
 
-            interpolator = interpolate.LinearNDInterpolator(ordered_coords, finalized_data[i])
-            interpolated_points = interpolator(transect_points)
+                # Transform data from SNAP projection to lat/lon
+                for i in range(len(ordered_coords)):
+                    ordered_coords[i] = transformer_to_lat_lon.transform(ordered_coords[i][0], ordered_coords[i][1])
 
-            # Iterate through each transect
-            # NOTE:For test purposes, replace len(transects) with a small number
-            for j in range(0, len(transects)):
+            else:
+                # Extract data without coords
+                modified_data, grb = crop_snap(ds, current_data, closest_x, closest_y)
 
-                # Create ID identifier for date/transect combo
-                temp_ID = str(final_date) + '_' + str(transects.iloc[j]['TransOrder'])
+            # Downscale surface temperature from hourly to daily #
+            downscaled_data = downscale_data(modified_data)
 
-                # Date and transect combo already present in dataframe
-                if (dataframe['ID'] == temp_ID).any():
+            # Reformat downscaled_data to work with ordered_coords for interpolation
+            finalized_data = finalize_data(downscaled_data)
+
+            # Initializing start date
+            strt_date = date(int(year), 1, 1)
+
+            # Interpolate this year's tsk data
+            for i in range(0, len(finalized_data)):
+                print(f'Processing {data_name} data: {i+1}/{len(finalized_data)}')
+
+                # Get date from day
+                day_num = str(i)
+                day_num.rjust(3 + len(day_num), '0')
+
+                res_date = strt_date + timedelta(days=int(day_num))
+                final_date = res_date.strftime("%m-%d-%Y")
+
+                interpolator = interpolate.LinearNDInterpolator(ordered_coords, finalized_data[i])
+                interpolated_points = interpolator(transect_points)
+
+                # Iterate through each transect
+                # NOTE:For test purposes, replace len(transects) with a small number
+
+                for j in range(0, len(transects)):
+                    # Create ID identifier for date/transect combo
+                    temp_ID = str(final_date) + '_' + str(transects.iloc[j]['TransOrder'])
+
                     # Update data value
                     dataframe.loc[dataframe['ID'] == temp_ID, data_name] = interpolated_points[j]
 
-                # Date and transect combo needs to be added
-                else:
-                    # Add data
-                    dataframe = dataframe.append(pd.DataFrame(
-                        {'date': [final_date], 'transect': [transects.iloc[j]['TransOrder']], 'ID': [temp_ID], data_name: [interpolated_points[j]]}))
-
-        return dataframe
+            return dataframe
+    return dataframe
 
 
 # Method to calculate wind speed and direction
 def calculate_wind_data(dataframe):
-    dataframe['Wind Speed'] = ((dataframe['u10']**2) + (dataframe['v10']**2)).pow(1./2)
-    dataframe['Wind Direction'] = np.arcsin(((0-dataframe['u10']) / dataframe['Wind Speed']).astype(np.float64))
+    dataframe['Wind Speed'] = ((dataframe['u10'] ** 2) + (dataframe['v10'] ** 2)).pow(1. / 2)
+    dataframe['Wind Direction'] = np.arcsin(((0 - dataframe['u10']) / dataframe['Wind Speed']).astype(np.float64))
     return dataframe
 
 
@@ -245,79 +271,119 @@ if __name__ == '__main__':
     # Transform center of Deering to SNAP coordinates
     mod_center = transformer_to_snap_proj.transform(REGION_CENTER[0], REGION_CENTER[1])
 
-    # Try to read in existing csv data file
-    try:
-        df = pd.read_csv(INPUT_FILEPATH)
-        # Remove leading column (index) from csv
-        df = df.iloc[:, 1:]
-        print('Existing data file found')
-        print('NOTE: Please make sure processed data has been removed from data directories')
-    except IOError:
-        print('No existing data file found')
-        columns = ['date', 'transect', 'ID', 'tsk', 'u10', 'v10', 'seaice', 'psfc']
-        df = pd.DataFrame(columns=columns)
+    # Create variable to store ordered coordinates
+    coords = []
+    year_list = []
 
     # Import surface skin temperature data
     tsk_data = read_data(TSK_FILEPATH)
     if tsk_data:
         print('Read in tsk data')
+        for ds in tsk_data:
+            # Grab year from data
+            year = int(ds.__dict__['reference_time'][0:4])
+            if year not in year_list:
+                year_list.append(year)
 
     # Import wind speed at 10m data (u component)
     u10_data = read_data(U10_FILEPATH)
     if u10_data:
         print('Read in u10 data')
+        for ds in u10_data:
+            # Grab year from data
+            year = int(ds.__dict__['reference_time'][0:4])
+            if year not in year_list:
+                year_list.append(year)
 
     # Import wind speed at 10m data (v component)
     v10_data = read_data(V10_FILEPATH)
     if v10_data:
         print('Read in v10 data')
+        for ds in v10_data:
+            # Grab year from data
+            year = int(ds.__dict__['reference_time'][0:4])
+            if year not in year_list:
+                year_list.append(year)
 
     # Import ice concentration data
     seaice_data = read_data(SEAICE_FILEPATH)
     if seaice_data:
         print('Read in seaice data')
+        for ds in seaice_data:
+            # Grab year from data
+            year = int(ds.__dict__['reference_time'][0:4])
+            if year not in year_list:
+                year_list.append(year)
 
     # Import surface pressure data
     psfc_data = read_data(PSFC_FILEPATH)
     if psfc_data:
         print('Read in psfc data')
+        for ds in psfc_data:
+            # Grab year from data
+            year = int(ds.__dict__['reference_time'][0:4])
+            if year not in year_list:
+                year_list.append(year)
 
-    # Read and process tsk data #
-    if tsk_data:
-        print('Processing tsk data')
-        df = process_data(tsk_data, 'tsk', df, transects)
-        print('tsk data processed. Writing current dataframe')
-        df.to_csv(OUTPUT_FILEPATH)
+    # Import 2m temperature data
+    t2_data = read_data(T2_FILEPATH)
+    if t2_data:
+        print('Read in t2 data')
+        for ds in t2_data:
+            # Grab year from data
+            year = int(ds.__dict__['reference_time'][0:4])
+            if year not in year_list:
+                year_list.append(year)
 
-    # Read and process u10 data #
-    if u10_data:
-        print('Processing u10 data')
-        df = process_data(u10_data, 'u10', df, transects)
-        print('u10 data processed. Writing current dataframe')
-        df.to_csv(OUTPUT_FILEPATH)
+    year_list.sort()
 
-    # Read and process v10 data #
-    if v10_data:
-        print('Processing v10 data')
-        df = process_data(v10_data, 'v10', df, transects)
-        print('v10 data processed. Writing current dataframe')
-        df.to_csv(OUTPUT_FILEPATH)
+    # Iterate through each year
+    for y in year_list:
 
-    # Read and process seaice data #
-    if seaice_data:
-        print('Processing seaice data')
-        df = process_data(seaice_data, 'seaice', df, transects)
-        print('seaice data processed. Writing current dataframe')
-        df.to_csv(OUTPUT_FILEPATH)
+        # Create dataframe
+        df = create_dataframe(transects, y)
 
-    # Read and process psfc data #
-    if psfc_data:
-        print('Processing psfc data')
-        df = process_data(psfc_data, 'psfc', df, transects)
-        print('psfc data processed. Writing current dataframe')
-        df.to_csv(OUTPUT_FILEPATH)
+        # Read and process tsk data #
+        if tsk_data:
+            print('Processing tsk data')
+            df = process_data(tsk_data, 'tsk', df, transects, coords, y)
+            print(f'{y} tsk data processed.')
 
-    print('Calculating wind speed and direction')
-    df = calculate_wind_data(df)
-    print('wind data processed. Writing final dataframe')
-    df.to_csv(OUTPUT_FILEPATH)
+        # Read and process u10 data #
+        if u10_data:
+            print('Processing u10 data')
+            df = process_data(u10_data, 'u10', df, transects, coords, y)
+            print(f'{y} u10 data processed.')
+
+        # Read and process v10 data #
+        if v10_data:
+            print('Processing v10 data')
+            df = process_data(v10_data, 'v10', df, transects, coords, y)
+            print(f'{y} v10 data processed.')
+
+        # Read and process seaice data #
+        if seaice_data:
+            print('Processing seaice data')
+            df = process_data(seaice_data, 'seaice', df, transects, coords, y)
+            print(f'{y} seaice data processed.')
+
+        # Read and process psfc data #
+        if psfc_data:
+            print('Processing psfc data')
+            df = process_data(psfc_data, 'psfc', df, transects, coords, y)
+            print(f'{y} psfc data processed.')
+
+        # Read and process t2 data #
+        if t2_data:
+            print('Processing t2 data')
+            df = process_data(t2_data, 't2', df, transects, coords, y)
+            print(f'{y} t2 data processed.')
+
+        print('Calculating wind speed and direction')
+        try:
+            df = calculate_wind_data(df)
+        except:
+            print(f'Unable to perform wind calculations for year {y}. Please check dataframe for missing data.')
+        print(f'{y} Wind data processed. Writing final dataframe')
+
+        df.to_csv(OUTPUT_FILEPATH + f'_{y}.csv')
