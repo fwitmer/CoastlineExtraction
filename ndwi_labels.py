a80ba2b450a3c1fbd36c23f1ec3e0ca115f7a1f5
Rawan Elframawy <86707527+rawann31@users.noreply.github.com>
2024-06-29 17:45:54 +0300

    Update ndwi_labels.py
    Enhance the code.
    Write Comments.
    Apply Gaussian filtering if we want.

--- ndwi_labels.py
+++ ndwi_labels.py
@@ -1,3 +1,4 @@
+# import libraries
 import rasterio as rio
 from rasterio import mask
 from rasterio.plot import show
@@ -44,7 +45,33 @@ def clip_shp(path_to_shp, boundary_geojson):
     out_path = path_name + shp_base + "_clipped.shp"
     shp_clipped.to_file(out_path)
 
-def get_ndwi_label(image_path, points_path, ksize = 100):
+
+# Gaussian blur parameters
+ksize_blur = (13, 13)  # Kernel size (must be positive and odd)
+sigmaX = 8      # Standard deviation in X direction
+sigmaY = 8      # Standard deviation in Y direction
+
+
+def get_ndwi_label(image_path, points_path, ksize = 100, blurring = True):
+    """
+    This function performs NDWI calculation and classification with optional Gaussian blurring.
+
+    Steps:
+    1. Read green and NIR bands to calculate NDWI.
+    2. Apply Gaussian blurring if required.
+    3. Match points CRS with Raster.
+    4. Create a buffer around each point (number of buffers depend on number of points in shapefile).
+    5. Mask the NDWI image with the specified buffer (sliding window).
+        5.1 out_image: same shape as original image with specified buffer.
+        5.2 out_image_clipped: clipped to the buffer.
+            If the entire buffer is inside the NDWI image, its shape should be (201,201).
+            If all or part of the sliding window is outside the NDWI image, the shape will be smaller.
+    6. Skip sliding windows that are not entirely inside the NDWI image.
+    7. Calculate threshold based on the clipped image.
+    8. Apply OR operations between `out_image` to form the final labeled image.
+    9. It is important to note that the NDWI threshold value may be less than 128.
+       Therefore, it is crucial to substitute no data with -1.
+    """
     # establish the ndwi calculation and copy metadata
     with rio.open(image_path, driver='GTiff') as src_raster:
         
@@ -57,18 +84,20 @@ def get_ndwi_label(image_path, points_path, ksize = 100):
         ndwi = (green - nir) / (green + nir)  # NDWI equation
         ndwi[np.isnan(ndwi)] = 0 # Sets any NaN values in the NDWI array to 0. (Dividing by zero => Nan Pixels)
         ndwi_profile = src_raster.profile # Copies the image profile (metadata).
-
+        
+        # Apply Gaussian blur
+        if blurring:
+            print("Gaussian Filtering Applied")
+            ndwi = cv2.GaussianBlur(ndwi, ksize_blur, sigmaX, sigmaY)
         
         # blank label layer
         label = np.zeros((src_raster.height, src_raster.width)).astype(np.uint8)
         src_CRS = src_raster.crs
+        
         # Getting pixel size for correct calculation of buffer.
         # This value express spatial resolution.
         pixel_size = abs(src_raster.transform[0]) 
         
-        figs, ax = plt.subplots(figsize=(12, 8))
-        show(ndwi, transform=src_raster.transform, ax=ax, cmap='gray')
-
     # preparing points for creating label masks
     points_shp = gpd.read_file(points_path)
     points_geom = points_shp.geometry
@@ -85,7 +114,6 @@ def get_ndwi_label(image_path, points_path, ksize = 100):
             # Create a buffer around the point
             buffer = point.buffer(ksize * pixel_size, cap_style=3)
             buffer_series = gpd.GeoSeries(buffer)
-            buffer_series.exterior.plot(ax=ax, color='red', linewidth=1)
 
             # writing NDWI to an in-memory dataset to use for masking
             ndwi_profile.update(count=1, nodata=0, dtype=rio.float32)
@@ -93,7 +121,7 @@ def get_ndwi_label(image_path, points_path, ksize = 100):
                 with memfile.open(**ndwi_profile) as mem_data:
                     mem_data.write_band(1, ndwi)
                 with memfile.open() as dataset:
-                    
+                
                     out_image, out_transform = mask(dataset, shapes=[buffer], nodata=-1, crop=False)
                     out_image = out_image[0]
                     out_image = (out_image * 127) + 128
@@ -116,30 +144,43 @@ def get_ndwi_label(image_path, points_path, ksize = 100):
                     threshold_window = np.where(out_image >= threshold_clipped, 1, 0).astype(np.uint8)
                     label = label | threshold_window.astype(np.uint8)
                         
-    print("Green max: {}".format(green.max()))
-    print("NIR max: {}".format(nir.max()))
-    print("NDWI max: {}".format(np.nanmax(ndwi)))
-    print("NDWI min: {}".format(np.nanmin(ndwi)))
+    print(f"Green min: {green.min():.2f}, Green max: {green.max():.2f}")
+    print(f"NIR min: {nir.min():.2f}, NIR max: {nir.max():.2f}")
+    print(f"NDWI min: {ndwi.min():.2f}, NDWI max: {ndwi.max():.2f}") # From -1 to +1
         
-    print("Total number of valid thresholds: {}".format(len(otsu_thresholds_clipped)))
-    print("Number of skipped windows: {}".format(skipped))
-    print("Actual thresholds (8-bit unsigned): \n{}".format(otsu_thresholds_clipped))
-    print("Average threshold value (8-bit unsigned): {}".format(np.mean(otsu_thresholds_clipped)))
-    print("Average threshold value (-1 to 1 NDWI range): {}".format((np.mean(otsu_thresholds_clipped) - 128) / 127))
+    print(f"Total number of valid thresholds: {len(otsu_thresholds_clipped)}")
+    print(f"Number of skipped windows: {skipped}")
+    
+    print(f"Actual thresholds (8-bit unsigned): {otsu_thresholds_clipped}")
+    print(f"Average threshold value (8-bit unsigned): {np.mean(otsu_thresholds_clipped)}")
+    print(f"Average threshold value (-1 to 1 NDWI range): {(np.mean(otsu_thresholds_clipped) - 128) / 127}")
 
-    print("\nLabel max: {}".format(np.nanmax(label)))
-    print("Label min: {}".format(np.nanmin(label)))
-    plt.imshow(label)
-    plt.show()
+    print(f"Label min: {np.nanmin(label)} , max: {np.nanmax(label)}")
 
     mean_threshold = np.mean(otsu_thresholds_clipped) + 10
     ndwi_8bit = ((ndwi * 127) + 128).astype(np.uint8)
     ndwi_classified = np.where(ndwi_8bit >= mean_threshold, 1, 0)
-    plt.imshow(ndwi_classified, cmap="gray")
+    
+    # Assuming 'ndwi' is your image data
+    plt.imshow(ndwi)
+    plt.title('NDWI image ')
+    plt.show()
+    
+    # Plotting images side by side
+    fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+
+    # Plot label image
+    axs[0].imshow(label)
+    axs[0].set_title('NDWI Classified with sliding window')
+    axs[0].axis('off')
+
+    # Plot NDWI classified image
+    axs[1].imshow(ndwi_classified, cmap="gray")
+    axs[1].set_title('NDWI Classified with mean threshold')
+    axs[1].axis('off')
+
     plt.show()
             
-    points_geom.plot(ax=ax, color='blue', markersize=5)
-    plt.show()
 
 
 boundary = {'type': 'Polygon',
