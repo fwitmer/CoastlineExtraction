3d29522724c3f7de897a7836ea37a48eb1974180
Rawan Elframawy <86707527+rawann31@users.noreply.github.com>
2024-07-01 01:30:54 +0300

    Update ndwi_labels.py

--- ndwi_labels.py
+++ ndwi_labels.py
@@ -47,81 +47,87 @@ def clip_shp(path_to_shp, boundary_geojson):
 
 
 # Gaussian blur parameters
-ksize_blur = (13, 13)  # Kernel size (must be positive and odd)
-sigmaX = 8      # Standard deviation in X direction
-sigmaY = 8      # Standard deviation in Y direction
+ksize_blur = (11, 11)  # Kernel size (must be positive and odd)
+sigmaX = 6      # Standard deviation in X direction
+sigmaY = 6      # Standard deviation in Y direction
+
+# majority factor indicate percent of sliding windows to segment pixel as water
+majority_threshold = 0.55
 
 
-def get_ndwi_label(image_path, points_path, ksize = 100, blurring = True):
+def get_ndwi_label(image_path, points_path, ksize=100, blurring=True):
     """
     This function performs NDWI calculation and classification with optional Gaussian blurring.
 
     Steps:
     1. Read green and NIR bands to calculate NDWI.
     2. Apply Gaussian blurring if required.
-    3. Match points CRS with Raster.
-    4. Create a buffer around each point (number of buffers depend on number of points in shapefile).
+    3. Match points CRS with raster.
+    4. Create a buffer around each point (number of buffers depends on the number of points in the shapefile).
     5. Mask the NDWI image with the specified buffer (sliding window).
-        5.1 out_image: same shape as original image with specified buffer.
+        5.1 out_image: same shape as the original image with the specified buffer.
         5.2 out_image_clipped: clipped to the buffer.
-            If the entire buffer is inside the NDWI image, its shape should be (201,201).
+            If the entire buffer is inside the NDWI image, its shape should be (201, 201).
             If all or part of the sliding window is outside the NDWI image, the shape will be smaller.
     6. Skip sliding windows that are not entirely inside the NDWI image.
     7. Calculate threshold based on the clipped image.
     8. Apply OR operations between `out_image` to form the final labeled image.
-    9. It is important to note that the NDWI threshold value may be less than 128.
+    9. Note that the NDWI threshold value may be less than 128.
        Therefore, it is crucial to substitute no data with -1.
+    10. Apply majority rule on the number of windows to segment pixels as water.
+    11. Concatenate the remaining sliding window images (unlabeled parts) from NDWI classified.
     """
-    # establish the ndwi calculation and copy metadata
+    # Establish the NDWI calculation and copy metadata
     with rio.open(image_path, driver='GTiff') as src_raster:
-        
-        green = src_raster.read(2).astype(np.float32) # Get the green band 
+        green = src_raster.read(2).astype(np.float32)  # Get the green band
         nir_num = src_raster.count  # Adjusting NIR band to 4 or 5 band images
-        nir = src_raster.read(nir_num).astype(np.float32) # Get NIR band
+        nir = src_raster.read(nir_num).astype(np.float32)  # Get NIR band
         
         np.seterr(divide='ignore', invalid='ignore')
         
         ndwi = (green - nir) / (green + nir)  # NDWI equation
-        ndwi[np.isnan(ndwi)] = 0 # Sets any NaN values in the NDWI array to 0. (Dividing by zero => Nan Pixels)
-        ndwi_profile = src_raster.profile # Copies the image profile (metadata).
+        ndwi[np.isnan(ndwi)] = 0  # Sets any NaN values in the NDWI array to 0. (Dividing by zero => NaN pixels)
+        ndwi_profile = src_raster.profile  # Copies the image profile (metadata).
         
         # Apply Gaussian blur
         if blurring:
             print("Gaussian Filtering Applied")
             ndwi = cv2.GaussianBlur(ndwi, ksize_blur, sigmaX, sigmaY)
         
-        # blank label layer
+        # Blank label layer
         label = np.zeros((src_raster.height, src_raster.width)).astype(np.uint8)
+        # Buffer matrix: each element represents the number of times a sliding window moves over a specific pixel.
+        buffer_numbers = np.zeros((src_raster.height, src_raster.width)).astype(np.uint8)
+        # Water count matrix: each pixel value represents the number of times pixels are labelled as water. 
+        water_count = np.zeros((src_raster.height, src_raster.width)).astype(np.uint8)
         src_CRS = src_raster.crs
         
         # Getting pixel size for correct calculation of buffer.
-        # This value express spatial resolution.
-        pixel_size = abs(src_raster.transform[0]) 
+        # This value expresses spatial resolution.
+        pixel_size = abs(src_raster.transform[0])
         
-    # preparing points for creating label masks
+    # Preparing points for creating label masks
     points_shp = gpd.read_file(points_path)
     points_geom = points_shp.geometry
-    points_geom = points_geom.set_crs(epsg=4326) # Set CRS to WGS84
-    points_geom = points_geom.to_crs(src_CRS) # Convert CRS to match the raster
+    points_geom = points_geom.set_crs(epsg=4326)  # Set CRS to WGS84
+    points_geom = points_geom.to_crs(src_CRS)  # Convert CRS to match the raster
 
-
-    otsu_thresholds_clipped = [] # Creating a holder for Otsu's threshold values for clipped images
-    skipped = 0 # Counter for skipped windows (Less than 201*201)
+    otsu_thresholds_clipped = []  # Creating a holder for Otsu's threshold values for clipped images
+    skipped = 0  # Counter for skipped windows (less than 201*201)
     
-    # processing each point found
+    # Processing each point found
     for multipoint in points_geom:
         for point in multipoint.geoms:
             # Create a buffer around the point
             buffer = point.buffer(ksize * pixel_size, cap_style=3)
             buffer_series = gpd.GeoSeries(buffer)
 
-            # writing NDWI to an in-memory dataset to use for masking
+            # Writing NDWI to an in-memory dataset to use for masking
             ndwi_profile.update(count=1, nodata=0, dtype=rio.float32)
             with MemoryFile() as memfile:
                 with memfile.open(**ndwi_profile) as mem_data:
                     mem_data.write_band(1, ndwi)
                 with memfile.open() as dataset:
-                
                     out_image, out_transform = mask(dataset, shapes=[buffer], nodata=-1, crop=False)
                     out_image = out_image[0]
                     out_image = (out_image * 127) + 128
@@ -131,22 +137,43 @@ def get_ndwi_label(image_path, points_path, ksize = 100, blurring = True):
                     out_image_clipped = out_image_clipped[0]
                     out_image_clipped = (out_image_clipped * 127) + 128
                     out_image_clipped = out_image_clipped.astype(np.uint8)
+                    
+                    # Mask array: mask pixels within the sliding window with 1, else 0
+                    mask_array = np.copy(out_image)
+                    mask_value = 1
+                    mask_array = np.where(mask_array == mask_value, 0, 1)
                 
-                # Skip buffering windows that part or all of it out of NDWI image
+                # Skip buffering windows that are partly or wholly out of the NDWI image
                 if out_image_clipped.shape[0] < 200 or out_image_clipped.shape[1] < 200:
                     skipped += 1
                     continue
                 
                 else:
-                    # Calculate otsu threshold based on clipped image
+                    # Calculate Otsu's threshold based on the clipped image
                     threshold_clipped, image_result_clipped = cv2.threshold(out_image_clipped, 0, 1, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
                     otsu_thresholds_clipped.append(threshold_clipped)
                     threshold_window = np.where(out_image >= threshold_clipped, 1, 0).astype(np.uint8)
-                    label = label | threshold_window.astype(np.uint8)
-                        
+                    label = label | threshold_window.astype(np.uint8)  # Labelled image with sliding windows 
+                    
+                    water_count = water_count + threshold_window
+                    buffer_numbers = buffer_numbers + mask_array
+    
+    # Labelled images based on majority sliding windows
+    majority_threshold = 0.5  # Example value; adjust based on your requirements
+    label_majority = np.where(water_count > (buffer_numbers * majority_threshold), 1, 0)
+    
+    # Labelled image based on mean threshold (one threshold)
+    mean_threshold = np.mean(otsu_thresholds_clipped) + 10
+    ndwi_8bit = ((ndwi * 127) + 128).astype(np.uint8)
+    ndwi_classified = np.where(ndwi_8bit >= mean_threshold, 1, 0)
+    
+    # Concatenate the remaining sliding window images (unlabeled parts) from NDWI classified.
+    sliding_windows = np.where(buffer_numbers > 0, 1, 0)
+    ndwi_concatenated = np.where(sliding_windows == 1, label, ndwi_classified)
+
     print(f"Green min: {green.min():.2f}, Green max: {green.max():.2f}")
     print(f"NIR min: {nir.min():.2f}, NIR max: {nir.max():.2f}")
-    print(f"NDWI min: {ndwi.min():.2f}, NDWI max: {ndwi.max():.2f}") # From -1 to +1
+    print(f"NDWI min: {ndwi.min():.2f}, NDWI max: {ndwi.max():.2f}")  # From -1 to +1
         
     print(f"Total number of valid thresholds: {len(otsu_thresholds_clipped)}")
     print(f"Number of skipped windows: {skipped}")
@@ -157,27 +184,36 @@ def get_ndwi_label(image_path, points_path, ksize = 100, blurring = True):
 
     print(f"Label min: {np.nanmin(label)} , max: {np.nanmax(label)}")
 
-    mean_threshold = np.mean(otsu_thresholds_clipped) + 10
-    ndwi_8bit = ((ndwi * 127) + 128).astype(np.uint8)
-    ndwi_classified = np.where(ndwi_8bit >= mean_threshold, 1, 0)
+    # Majority sliding windows
+    label_majority = np.where(water_count > (buffer_numbers * majority_threshold), 1, 0)
     
     # Assuming 'ndwi' is your image data
     plt.imshow(ndwi)
-    plt.title('NDWI image ')
+    plt.title('NDWI image')
     plt.show()
     
     # Plotting images side by side
-    fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+    fig, axs = plt.subplots(2, 2, figsize=(20, 20))
+    
+    # Plot NDWI classified image (based on one mean threshold)
+    axs[0, 0].imshow(ndwi_classified)
+    axs[0, 0].set_title('NDWI Classified with mean threshold')
+    axs[0, 0].axis('off')
 
-    # Plot label image
-    axs[0].imshow(label)
-    axs[0].set_title('NDWI Classified with sliding window')
-    axs[0].axis('off')
+    # Plot labelled image (based on sliding windows)
+    axs[0, 1].imshow(label)
+    axs[0, 1].set_title('NDWI Classified with sliding window')
+    axs[0, 1].axis('off')
 
-    # Plot NDWI classified image
-    axs[1].imshow(ndwi_classified, cmap="gray")
-    axs[1].set_title('NDWI Classified with mean threshold')
-    axs[1].axis('off')
+    # Plot labelled image (based on majority sliding windows)
+    axs[1, 0].imshow(label_majority)
+    axs[1, 0].set_title('NDWI Classified with majority sliding window')
+    axs[1, 0].axis('off')
+    
+    # Plot NDWI classified concatenated between majority sliding windows with one mean threshold
+    axs[1, 1].imshow(ndwi_concatenated)
+    axs[1, 1].set_title('NDWI Concatenated')
+    axs[1, 1].axis('off')
 
     plt.show()
             
